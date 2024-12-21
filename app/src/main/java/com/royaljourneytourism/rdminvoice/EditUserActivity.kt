1899f1e2aca7f5caf47c6c353c14503d7a20a1f4
Abdul-Rehman-Ali <chabdulrehman1039@gmail.com>
2024-12-21 11:36:07 +0500

    Eit User

--- app/src/main/java/com/royaljourneytourism/rdminvoice/EditUserActivity.kt
+++ app/src/main/java/com/royaljourneytourism/rdminvoice/EditUserActivity.kt
@@ -5,13 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.royaljourneytourism.rdminvoice.databinding.ActivityEditUserBinding
 
 class EditUserActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_user)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Initialize View Binding
+        binding = ActivityEditUserBinding.inflate(layoutInflater)
+
+        // Set the content view to the binding root
+        setContentView(binding.root)
+
+        // Handle window insets for padding adjustments
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
